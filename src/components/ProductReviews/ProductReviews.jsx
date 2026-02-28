ab70fc12f2cd150c29fbc075188a6aa21d7d9e28
caglayan <caglayanustaa@gmail.com>
2026-02-28 22:24:37 +0300

    reviews done css

--- src/components/ProductReviews/ProductReviews.jsx
+++ src/components/ProductReviews/ProductReviews.jsx
@@ -1,34 +1,38 @@
-import { useSelector } from "react-redux"
-import { selectorOneProduct } from "../../redux/products/selectors"
-
+import { useSelector } from "react-redux";
+import { selectorOneProduct } from "../../redux/products/selectors";
+import styles from "./ProductReviews.module.css";
 
 const ProductReviews = () => {
-
   const product = useSelector(selectorOneProduct);
 
-  console.log(product)
+  console.log(product);
 
   return (
-    <div>
+    <div className={styles.reviews}>
       <ul>
-        {
-          product.reviews.map((item, index) => (
-            <li key={index}>
-              <p>
-                {item.reviewer_name}
-              </p>
-              <p>
+        {product.reviews.map((item, index) => (
+          <li key={index}>
+            <div className={styles.reviewer}>
+              <p className={styles.initial}>
                 {
-                  item.reviewer_rating
+                  item.reviewer_name[0].toUpperCase()
                 }
               </p>
-              <p>{item.comment}</p>
-            </li>
-          ))
-        }
+              <div>
+                <p style={{marginBottom:10}}>{item.reviewer_name}</p>
+                <p>
+                  {[...Array(item.reviewer_rating)].map((_, i) => (
+                    <img key={i} src="../../../public/star.svg" width="20" height="20" alt="star" />
+                  ))}
+                </p>
+              </div>
+            </div>
+            <p className={styles.comment}>{item.comment}</p>
+          </li>
+        ))}
       </ul>
     </div>
-  )
-}
+  );
+};
 
-export default ProductReviews
+export default ProductReviews;
